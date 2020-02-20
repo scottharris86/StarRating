5396421aba926cdcd61f73ad88ecdcbae1461313
scottharris86 <scottharrisdev@gmail.com>
2020-02-20 16:14:48 -0500

    Initial Commit

---
+++ StarRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarRating
+//
+//  Created by scott harris on 2/20/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
