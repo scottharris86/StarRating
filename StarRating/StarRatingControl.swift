984174692fabe6cab0cade64f345f1c0aa45c8bf
scottharris86 <scottharrisdev@gmail.com>
2020-02-20 17:49:31 -0500

    built control view

--- StarRating/StarRatingControl.swift
+++ StarRating/StarRatingControl.swift
@@ -8,8 +8,55 @@
 
 import UIKit
 
+
+@IBDesignable
 class StarRatingControl: UIControl {
+    
     // MARK: - Properties
+    
     var value: Int = 1
+    private let componentDimension: CGFloat = 40.0
+    private let componentCount = 5
+    private let componentActiveColor: UIColor = .black
+    private let componentInactiveColor: UIColor = .gray
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
+        var stars: [UILabel] = []
+        for i in 1...5 {
+            let label = UILabel()
+            let padding = CGFloat(8.0 * CGFloat(i))
+               label.frame = CGRect(x: padding + (componentDimension * CGFloat(i - 1)), y: 0, width: componentDimension, height: componentDimension)
+            
+            let star = "⭑"
+            label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+            label.textAlignment = .center
+            label.text = star
+            if i == 1 {
+                label.textColor = componentActiveColor
+            } else {
+                label.textColor = componentInactiveColor
+            }
+            label.tag = i
+            stars.append(label)
+            addSubview(label)
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
     
 }
